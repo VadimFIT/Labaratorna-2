103902363500d3353264c33745aa55e7a36318a0
VadimFIT <V.Kurdya.FIT.121.20@knute.edu.ua>
2022-05-26 10:50:54 +0300

    first commit

---
+++ laba 2_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDict<string, int> Ages = new MyDict<string, int>();
+            Ages.Add("Ihor", 19);
+            Console.WriteLine(Ages["Ihor"]);
+
+        }
+    }
+}
